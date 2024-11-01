ec3d66aee31b330dc4ee7a51f3023d6ca4882de9
matthewgouyer <mgouyer@live.com>
2024-11-01 11:47:08 -0700

    Made Define Transaction Type Page. Saves to data/transactiontypes.csv UI for page could use some work.
    Also deleted data folder in resources because it wasn't being used.

---
+++ CS151_A3/src/application/controller/AddTransactionTypeController.java
@@ -0,0 +1,40 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class AddTransactionTypeController {
+
+    @FXML private TextField transactionTypeField;
+    @FXML private Label validationLabel;
+
+    private static final String DATA_FOLDER = "data";
+    private static final String FILE_NAME = "transactiontypes.csv";
+
+    @FXML
+    private void saveTransactionType() {
+        String transactionType = transactionTypeField.getText();
+        
+        if (transactionType.isEmpty()) {
+            validationLabel.setText("Transaction type is required.");
+            return;
+        }
+
+        // Save transaction type to file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(DATA_FOLDER, FILE_NAME).toString(), true))) {
+            writer.write(transactionType);
+            writer.newLine();
+            validationLabel.setText("Transaction type saved.");
+        } catch (IOException e) {
+            validationLabel.setText("Error saving transaction type.");
+            e.printStackTrace();
+        }
+    }
+}
--- CS151_A3/src/application/controller/MainController.java
+++ CS151_A3/src/application/controller/MainController.java
@@ -1,13 +1,7 @@
 package application.controller;
 
 import java.io.IOException;
-
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.Node;
 import java.net.URL;
-import java.time.LocalDate;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
@@ -15,56 +9,49 @@ import javafx.scene.layout.HBox;
 
 public class MainController {
 
-	@FXML HBox mainBox;
-	
-	//loading content at launch
-	@FXML public void initialize() {
-		showContent1Op();
-	}
+    @FXML HBox mainBox;
 
-	@FXML public void showContent1Op() {
-		URL url = getClass().getClassLoader().getResource("view/Content1.fxml");
-		
-		try {
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 1) {
-				mainBox.getChildren().remove(1);
-			}
-			
-			mainBox.getChildren().add(pane1);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
+    // Loading content at launch
+    @FXML public void initialize() {
+        showContent1Op();
+    }
 
-	@FXML public void showContent2Op() {
-		
-		URL url = getClass().getClassLoader().getResource("view/Content2.fxml");
-		
-		try {
-			AnchorPane pane2 = (AnchorPane) FXMLLoader.load(url);
-			
-			if (mainBox.getChildren().size() > 1) {
-				mainBox.getChildren().remove(1);
-			}
-			
-			mainBox.getChildren().add(pane2);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	}
-	
-	
+    @FXML public void showContent1Op() {
+        URL url = getClass().getClassLoader().getResource("view/Content1.fxml");
+        try {
+            AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+            if (mainBox.getChildren().size() > 1) {
+                mainBox.getChildren().remove(1);
+            }
+            mainBox.getChildren().add(pane1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML public void showContent2Op() {
+        URL url = getClass().getClassLoader().getResource("view/Content2.fxml");
+        try {
+            AnchorPane pane2 = (AnchorPane) FXMLLoader.load(url);
+            if (mainBox.getChildren().size() > 1) {
+                mainBox.getChildren().remove(1);
+            }
+            mainBox.getChildren().add(pane2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	
-	
+    @FXML public void goToAddTransactionTypes() {
+        URL url = getClass().getClassLoader().getResource("view/Content4.fxml");
+        try {
+            AnchorPane pane3 = (AnchorPane) FXMLLoader.load(url);
+            if (mainBox.getChildren().size() > 1) {
+                mainBox.getChildren().remove(1);
+            }
+            mainBox.getChildren().add(pane3);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
